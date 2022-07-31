287f1c37deeb61a06c78ec66d0824797a905b8d9
NetwarSystem <nealr@pm.me>
2022-07-31 05:30:24 -0700

    Just getting started

---
+++ tgarchive/example/static/main.js
@@ -0,0 +1,49 @@
+(function() {
+	
+		document.addEventListener("DOMContentLoaded", function(event) {
+		const observer = window.lozad('.lozad'/*, {
+			load: function(el) {
+				console.log('loading element');
+				el.src = el.getAttribute('data-src');
+			}
+		}*/);
+		observer.observe();
+	});
+
+
+	// Hide the open burger menu when clicking nav links. 
+	const burger = document.querySelector("#burger");
+	document.querySelectorAll(".timeline a, .dayline a").forEach((e) => {
+		e.onclick = () => {
+			burger.checked = false;
+		};
+	});
+
+	// Change page anchor on scrolling past days.
+	let is = null;
+	document.onscroll = () => {
+		if (is) {
+			window.clearTimeout(is);
+		}
+		is = window.setTimeout(() => {
+			const days = document.querySelectorAll(".messages .day");
+			let lastID = days[0].id;
+			days.forEach((el) => {
+				if (el.getBoundingClientRect().top < 10) {
+					lastID = el.id;
+				} else {
+					return false;
+				}
+			});
+
+			if (lastID) {
+				history.replaceState({}, "", `#${lastID}`);
+				let q = document.querySelector(".dayline .selected")
+				if (q) {
+					q.classList.remove("selected");
+				}
+				document.querySelector(`.dayline .day-${lastID}`).classList.add("selected");
+			}
+		}, 100);
+	};
+})();
